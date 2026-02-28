6e939085a18ddaf3cfc0f65b08a86ac8bf78408d
James Ross <james@flyingrobots.dev>
2026-02-28 10:20:23 -0800

    chore(release): v5.2.2

--- bin/git-cas.js
+++ bin/git-cas.js
@@ -18,7 +18,7 @@ const getJson = () => program.opts().json;
 program
   .name('git-cas')
   .description('Content Addressable Storage backed by Git')
-  .version('5.2.1')
+  .version('5.2.2')
   .option('-q, --quiet', 'Suppress progress output')
   .option('--json', 'Output results as JSON');
 
